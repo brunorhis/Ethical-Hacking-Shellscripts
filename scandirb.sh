e27eacb43aaf918bf3f625fa91175eeacec2f821
Bruno Rhis <92235639+brunorhis@users.noreply.github.com>
2023-03-14 11:06:33 -0300

    Add files via upload

---
+++ scandirb.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+read -p "Digite o alvo: " target
+read -p "Digite o caminho da wordlist: " wordlist
+read -p "Escolha o tipo de scan (1-2-3-4-5): 
+    1 - Varredura de diretórios padrão
+    2 - Varredura de diretórios com arquivos PHP
+    3 - Varredura de diretórios com arquivos HTML
+    4 - Varredura de diretórios com arquivos JS
+    5 - Varredura de diretórios com arquivos JSP
+" scan_type
+
+case $scan_type in
+    1)
+        scan_name="padrao"
+        ;;
+    2)
+        scan_name="php"
+        ;;
+    3)
+        scan_name="html"
+        ;;
+    4)
+        scan_name="js"
+        ;;
+    5)
+        scan_name="jsp"
+        ;;
+    *)
+        echo "Tipo de scan inválido!"
+        exit 1
+        ;;
+esac
+
+echo "Executando o scan em $target com a wordlist $wordlist e o tipo $scan_name"
+
+dirb $target $wordlist -o resultado_$scan_name.txt
